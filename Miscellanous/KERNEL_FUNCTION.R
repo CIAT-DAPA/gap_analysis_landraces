389f019a7f51cb8d404aa0055bea33cb0e5a50aa
Chrystian <chrystiam.sossa@gmail.com>
2018-02-14 09:08:22 -0500

    Added Standardize version dividing by the max value of the kernel to get values from 0 to 1

--- Miscellanous/KERNEL_FUNCTION.R
+++ Miscellanous/KERNEL_FUNCTION.R
@@ -9,8 +9,8 @@ require(spatstat);require(raster);require(sp);require(adehabitatHR);require(SDMT
 
 ###############
 
-RASTER_KERNEL<-function(species,mask,occurrences,out_dir,spatstat){
-  cat("Reading mask and occurrences","\n")
+raster_kernel<-function(species,mask,occurrences,out_dir,spatstat,scale){
+  cat("Reading mask and occurrences for: ",species,"\n")
   
   ### Reading occurrences 
   sp::coordinates(occurrences)<-~lon+lat
@@ -18,11 +18,11 @@ RASTER_KERNEL<-function(species,mask,occurrences,out_dir,spatstat){
   
   if(spatstat==TRUE){
     ver<-"SPAT"}else{
-      ver<-"ADE"  
+      ver<-""  
     }
   #####
   if(spatstat==TRUE){
-    cat("Using Kernel Classical version!","\n")
+    cat("Using Kernel Classical version for: ",species,"\n")
     
     
     ### Transforming mask to owin object 
@@ -35,19 +35,32 @@ RASTER_KERNEL<-function(species,mask,occurrences,out_dir,spatstat){
     occurrences_ppp <- spatstat::ppp(x=occurrences@coords[,1],y=occurrences@coords[,2],window=w)
     
     ### Calculating Cross Validated Bandwidth Selection for Kernel Density usingh MSE
-    cat("Cross Validated Bandwidth Selection for Kernel Density usingh MSE","\n")
+    cat("Cross Validated Bandwidth Selection for Kernel Density usingh MSE for: ",species,"\n")
     bw_dig <- spatstat::bw.diggle(occurrences_ppp)
     
     ### Calculating density
-    cat("Calculating Kernel density using Cross Validated Bandwidth Selection for Kernel Density parameter","\n")
+    cat("Calculating Kernel density using Cross Validated Bandwidth Selection for Kernel Density parameter for: ",species,"\n")
     
     kernel <- spatstat::density.ppp(x=occurrences_ppp,sigma=bw_dig,at="pixels",verbose=F,diggle=T)
     kernel <- raster::raster(kernel);rm(w,bw_dig);gc()
     
-  }else{
-    cat("Using Adehabitat Kernel UD version!","\n")
+    if(scale==T){
+      kernel< - kernel/max(kernel[],na.rm=T)
+    }else{
+      kernel <- kernel
+         }
+  
+    }else{
+    cat("Using Adehabitat Kernel UD version for: ",species,"\n")
     rAsc <- asc.from.raster(mask);rAsc <- adehabitatMA::asc2spixdf(rAsc);gc()
     kernel <- adehabitatHR::kernelUD(occurrences,h="href",grid=rAsc)
+    
+    if(scale==T){
+      kernel< - kernel/max(kernel[],na.rm=T)
+    }else{
+      kernel <- kernel
+    }
+    
     res <- as.data.frame(kernel[[1]])
     sp::coordinates(res) <- coordinates(kernel)
     sp::gridded(res) <- TRUE
@@ -67,7 +80,7 @@ RASTER_KERNEL<-function(species,mask,occurrences,out_dir,spatstat){
   kernel <- kernel * mask
   
   ### Saving raster object
-  cat("Saving raster object to be used","\n")
+  cat("Saving raster object to be used for: ",species,"\n")
   raster::writeRaster(kernel,paste0(out_dir,"/",species,"_",ver,"_Kernel.tif"))
   
   return(kernel)
@@ -81,9 +94,9 @@ RASTER_KERNEL<-function(species,mask,occurrences,out_dir,spatstat){
 
 
 
-# species <- "Andean"
-# 
-# ### Calling paths to perform analysis
+speciesList <- c("Mesoamerican","Andean")
+
+### Calling paths to perform analysis
 # root <- "U:"
 # input_dir <- paste0(root,"/","Input_data")
 # sdm_dir <- paste0(input_dir,"/","SDMs")
@@ -91,10 +104,16 @@ RASTER_KERNEL<-function(species,mask,occurrences,out_dir,spatstat){
 # ### Reading mask
 # mask <- raster::raster(paste0(input_dir,"/","mask_wb_c_ant.tif"))
 # 
+# 
+# ### Running for a Species list
+# lapply(1:length(speciesList), function(i){
+# 
+# species=speciesList[[i]]
+# 
 # ### Reading occurrences
 # occurrences <- read.csv(paste0(sdm_dir,"/","occurrences","/","occ_",species,".csv"),header=T)
 # occurrences <- occurrences[,c("lon","lat")]
-
-
-#x<-RASTER_KERNEL(species,mask,occurrences,out_dir,spatstat=F)
-#x<-RASTER_KERNEL(species,mask,occurrences,out_dir,spatstat=T)
+# 
+# x<-RASTER_KERNEL(species,mask,occurrences,out_dir,spatstat=F,scale=T)
+# })
+# #x<-RASTER_KERNEL(species,mask,occurrences,out_dir,spatstat=F,standardize=T)
