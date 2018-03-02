53a69682f432499072b9a9c61c5fdf87646263fd
Chrystian <chrystiam.sossa@gmail.com>
2018-03-02 13:55:09 -0500

    Added option to 1- kernel or just use as is

--- Miscellanous/kernel_indicator.R
+++ Miscellanous/kernel_indicator.R
@@ -1,14 +1,21 @@
 #Kernel indicator
 
-kernel_indicator <- function(kernel,friction,model_outDir,gap_outDir){
+kernel_indicator <- function(kernel,friction,model_outDir,gap_outDir,reverse){
   #loading Maxent Model
   model <- raster(paste(model_outDir,"/",occName,"_prj_median.tif",sep=""))
   #loading friction surface
   friction <-  raster(friction)
+  #Transforming friction in 0 - 1 variable
+  friction <- friction/max(friction[],na.rm=T)
   #Transforming Kernel in 0 - 1 variable
-  kernel2 <- (kernel/max(kernel[],na.rm=T))
-  # Returning inverse Kernel
-  kernel2 <- 1 - kernel2
+  kernel2 <- kernel/max(kernel[],na.rm=T)
+  if(reverse==T){
+    # Returning inverse Kernel
+    kernel2 <- (1 - kernel2) 
+    } else {
+    kernel2 <- kernel2 
+    }
+
   #Gathering first Indicator (kernel * Maxent probability)
   mm1 <- kernel2 * model
   #Gathering second Indicator (First indicator * Friction surface)
