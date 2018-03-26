f02119d869f8d4b1f2034cabc27b51100e102b4e
Julian Ramirez-Villegas <julian@MacBook-Air-3.local>
2018-03-26 04:26:59 -0500

    add env_distance.R to gap_methods

---
+++ 03_gap_methods/env_distance.R
@@ -0,0 +1,9 @@
+#Julian Ramirez-Villegas
+#March 2018
+
+#calculate environmental distance to closest accession for each evironmental cluster
+
+#working directory
+wd <- "~/nfs/workspace_cluster_9/gap_analysis_landraces"
+
+
