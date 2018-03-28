d53eb6a1520873366a4959262753c6f1f4242a29
andresk159 <a.c.mendez@cgiar.org>
2018-03-28 10:23:35 -0500

    delanuay

--- finding_gaps/delanuay_proposal.R
+++ finding_gaps/delanuay_proposal.R
@@ -2,7 +2,7 @@
 suppressMessages(if(!require(pacman)){install.packages('pacman'); library(pacman)} else {library(pacman)})
 pacman::p_load(dplyr, psych, tm, raster, rgdal, rasterVis, rgeos, deldir, sp, tidyverse, FactoMineR, factoextra, ggdendro, rlang, fastcluster, sf ) 
 
-wmask <- raster("\\dapadfs\Workspace_cluster_9\gap_analysis_landraces\Input_data\mask_wb_c_ant.tif")
+wmask <- raster("//dapadfs/Workspace_cluster_9/gap_analysis_landraces/Input_data/mask_wb_c_ant.tif")
 
 delanuay <- shapefile("//dapadfs/Workspace_cluster_9/gap_analysis_landraces/Input_data/_datosAndres/delanuay/delanuay_andres.shp")
 delanuay@bbox <- as.matrix(extent(wmask))
@@ -11,10 +11,18 @@ delanuay@bbox <- as.matrix(extent(wmask))
 r <- raster()
 extent(r) <- extent(wmask)
 crs(r) <- crs(wmask)
+res(r) <- res(wmask)
+r[is.na(r[])]<- 0
 
 del <- st_sf(dealnuay[1])
 st_intersection(del, r)
 
+cr <- crop( wmask , delanuay[1563,] )
+rr <- mask(cr, delanuay[1563,])
 
-cr <- crop( r , delanuay[200,] )
-rr <- mask(cr, delanuay[1860,])
+xy <- SpatialPoints(delanuay@data[1563, 2:3])
+crs(xy) <- crs(wmask)
+dRas <- distanceFromPoints(rr, xy )
+
+f_ras <-  dRas + rr
+plot(f_ras)
