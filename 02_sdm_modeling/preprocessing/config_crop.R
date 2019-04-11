8cfa7e938b4a7da2819c28b319b5073a53fafeaf
Mendez <A.C.MENDEZ@cgiar.org>
2019-04-11 16:07:10 -0500

    added recursive = T to dir.create

--- 02_sdm_modeling/preprocessing/config_crop.R
+++ 02_sdm_modeling/preprocessing/config_crop.R
@@ -17,14 +17,14 @@ if(length(level_1) > 0){
 lapply(1:length(level_1), function(i){
   # Input data directories for crop
   # class_dir <- paste(crop_dir, "/lvl_1/classification", sep = ""); if(!file.exists(class_dir)){dir.create(class_dir)}
-  class_dir_results <- paste(crop_dir_results, "/lvl_1/classification", sep = ""); if(!file.exists(class_dir_results)){dir.create(class_dir_results)}
-  level_dir_i <- paste(crop_dir, "/lvl_1/", level_1[[i]], sep = ""); if (!file.exists(level_dir_i)) {dir.create(level_dir_i)}
-  occDir <- paste(level_dir_i, "/occurrences", sep = ""); if (!file.exists(occDir)) {dir.create(occDir)}
-  backDir <- paste(level_dir_i, "/background", sep = ""); if (!file.exists(backDir)) {dir.create(backDir)}
-  swdDir <- paste(level_dir_i, "/swd", sep = ""); if (!file.exists(swdDir)) {dir.create(swdDir)}
+  class_dir_results <- paste(crop_dir_results, "/lvl_1/classification", sep = ""); if(!file.exists(class_dir_results)){dir.create(class_dir_results, recursive = TRUE)}
+  level_dir_i <- paste(crop_dir, "/lvl_1/", level_1[[i]], sep = ""); if (!file.exists(level_dir_i)) {dir.create(level_dir_i, recursive = TRUE)}
+  occDir <- paste(level_dir_i, "/occurrences", sep = ""); if (!file.exists(occDir)) {dir.create(occDir, recursive = TRUE)}
+  backDir <- paste(level_dir_i, "/background", sep = ""); if (!file.exists(backDir)) {dir.create(backDir, recursive = TRUE)}
+  swdDir <- paste(level_dir_i, "/swd", sep = ""); if (!file.exists(swdDir)) {dir.create(swdDir, recursive = TRUE)}
  
   # result directories for crop
-  level_dir_r <- paste(crop_dir_results, "/lvl_1/", level_1[[i]], sep = ""); if (!file.exists(level_dir_r)) {dir.create(level_dir_r)}
+  level_dir_r <- paste(crop_dir_results, "/lvl_1/", level_1[[i]], sep = ""); if (!file.exists(level_dir_r)) {dir.create(level_dir_r, recursive = TRUE)}
   })
 } else {
   cat("Level 1 not available","\n")
@@ -34,13 +34,13 @@ if(length(level_2) > 0){
 lapply(1:length(level_2), function(i){
   # Input data directories for crop
   # class_dir <- paste(crop_dir,"/","lvl_2","/","classification",sep=""); if(!file.exists(class_dir)){dir.create(class_dir)}
-  class_dir_results <- paste(crop_dir_results, "/lvl_2/classification", sep = ""); if(!file.exists(class_dir_results)){dir.create(class_dir_results)}
-  level_dir_i <- paste(crop_dir, "/lvl_2/", level_2[[i]], sep = ""); if (!file.exists(level_dir_i)) {dir.create(level_dir_i)}
-  occDir <- paste(level_dir_i, "/occurrences", sep = ""); if (!file.exists(occDir)) {dir.create(occDir)}
-  backDir <- paste(level_dir_i, "/background", sep = ""); if (!file.exists(backDir)) {dir.create(backDir)}
-  swdDir <- paste(level_dir_i, "/swd", sep = ""); if (!file.exists(swdDir)) {dir.create(swdDir)}
+  class_dir_results <- paste(crop_dir_results, "/lvl_2/classification", sep = ""); if(!file.exists(class_dir_results)){dir.create(class_dir_results, recursive = TRUE)}
+  level_dir_i <- paste(crop_dir, "/lvl_2/", level_2[[i]], sep = ""); if (!file.exists(level_dir_i)) {dir.create(level_dir_i, recursive = TRUE)}
+  occDir <- paste(level_dir_i, "/occurrences", sep = ""); if (!file.exists(occDir)) {dir.create(occDir, recursive = TRUE)}
+  backDir <- paste(level_dir_i, "/background", sep = ""); if (!file.exists(backDir)) {dir.create(backDir, recursive = TRUE)}
+  swdDir <- paste(level_dir_i, "/swd", sep = ""); if (!file.exists(swdDir)) {dir.create(swdDir, recursive = TRUE)}
   # result directories for crop
-  level_dir_r <- paste(crop_dir_results,"/lvl_2/", level_2[[i]], sep = ""); if (!file.exists(level_dir_r)) {dir.create(level_dir_r)}
+  level_dir_r <- paste(crop_dir_results,"/lvl_2/", level_2[[i]], sep = ""); if (!file.exists(level_dir_r)) {dir.create(level_dir_r, recursive = TRUE)}
   })
 } else {
   cat("Level 2 not available","\n")
@@ -50,13 +50,13 @@ if(length(level_3) > 0){
   lapply(1:length(level_3), function(i){
     # Input data directories for crop
     # class_dir <- paste(crop_dir, "/lvl_3/classification", sep = ""); if(!file.exists(class_dir)){dir.create(class_dir)}
-    class_dir_results <- paste(crop_dir_results, "/lvl_3/classification", sep = ""); if(!file.exists(class_dir_results)){dir.create(class_dir_results)}
-    level_dir_i <- paste(crop_dir, "/lvl_3/", level_3[[i]], sep=""); if (!file.exists(level_dir_i)) {dir.create(level_dir_i)}
-    occDir <- paste(level_dir_i, "/occurrences", sep = ""); if (!file.exists(occDir)) {dir.create(occDir)}
-    backDir <- paste(level_dir_i, "/background", sep = ""); if (!file.exists(backDir)) {dir.create(backDir)}
-    swdDir <- paste(level_dir_i, "/swd", sep = ""); if (!file.exists(swdDir)) {dir.create(swdDir)}
+    class_dir_results <- paste(crop_dir_results, "/lvl_3/classification", sep = ""); if(!file.exists(class_dir_results)){dir.create(class_dir_results, recursive = TRUE)}
+    level_dir_i <- paste(crop_dir, "/lvl_3/", level_3[[i]], sep=""); if (!file.exists(level_dir_i)) {dir.create(level_dir_i, recursive = TRUE)}
+    occDir <- paste(level_dir_i, "/occurrences", sep = ""); if (!file.exists(occDir)) {dir.create(occDir, recursive = TRUE)}
+    backDir <- paste(level_dir_i, "/background", sep = ""); if (!file.exists(backDir)) {dir.create(backDir, recursive = TRUE)}
+    swdDir <- paste(level_dir_i, "/swd", sep = ""); if (!file.exists(swdDir)) {dir.create(swdDir, recursive = TRUE)}
     # result directories for crop
-    level_dir_r <- paste(crop_dir_results, "/lvl_3/", level_3[[i]], sep = ""); if (!file.exists(level_dir_r)) {dir.create(level_dir_r)}
+    level_dir_r <- paste(crop_dir_results, "/lvl_3/", level_3[[i]], sep = ""); if (!file.exists(level_dir_r)) {dir.create(level_dir_r, recursive = TRUE)}
    }) 
   }
 }
